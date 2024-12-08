a045cf3ada775d781e52bb76eb62633f262ceace
ian-ledig <ian.ledigfr@gmail.com>
2024-12-08 17:51:36 +0100

    adding mail sender for contact form

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -1,12 +1,37 @@
 import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const { email, subject, message } = body;
+    try{
+        const body = await req.json();
+        const { email, subject, message } = body;
 
-  if (!email || !subject || !message) {
-    return NextResponse.json({ message: 'All fields are required' }, { status: 400 });
-  }
+        if (!email || !subject || !message) {
+            return NextResponse.json({ message: 'All fields are required' }, { status: 400 });
+        }
 
-  return NextResponse.json({ message: 'Message received successfully!' });
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: Number(process.env.EMAIL_PORT),
+            auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: 'ian.ledigfr@gmail.com',
+            subject: `Portfolio Contact Form: ${subject}`,
+            text: `Message from: ${email}\n\n${message}`,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log('Message sent: %s', info.messageId);
+        return NextResponse.json({ message: 'Message received successfully!' }, { status: 200 });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
 }
